b4a3ee3d6d8bd0e42b047f7eb92e0122407b8dff
SvetlankaKuzyaka <svetlanka-kuznecov@yandex.ru>
2019-03-22 12:37:39 +0300

    add half-solve

--- src/max-heap.js
+++ src/max-heap.js
@@ -2,48 +2,80 @@ const Node = require('./node');
 
 class MaxHeap {
 	constructor() {
-		
+	  this.root = null;
+		this.parentNodes = [];
+		this.size = 0;
 	}
 
 	push(data, priority) {
-		
+		var new_node = new Node(data, priority);
+		this.insertNode(new_node);
+		this.shiftNodeUp(new_node);
+		this.size += 1;
 	}
 
 	pop() {
-		
+		if (this.size == 0) return;
+
+		this.size -= 1;
 	}
 
 	detachRoot() {
-		
+		var root_detach = this.root;
+		if (this.parentNodes[0] == root_detach) {this.parentNodes.shift();}
+		this.root = null;
+		return root_detach;
 	}
 
 	restoreRootFromLastInsertedNode(detached) {
-		
+
 	}
 
 	size() {
-		
+		return this.size;
 	}
 
 	isEmpty() {
-		
+		if (this.size == 0) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	clear() {
-		
+		this.root = null;
+		this.parentNodes = [];
+		this.size = 0;
 	}
 
 	insertNode(node) {
-		
+	  if (!this.root) {this.root = node; this.parentNodes.push(node);}
+		var parent_node = this.parentNodes[0];
+		node.parent = parent_node;
+		if (!parent_node.left) {
+			parent_node.left = node;
+			this.parentNodes.push(node);
+		} else {
+			parent_node.right = node;
+			this.parentNodes.shift();
+			this.parentNodes.push(node);
+		}
 	}
 
 	shiftNodeUp(node) {
-		
+	//	while (node.priority > node.parent.priority) {
+		//	node.swapWithParent();
+	//	}
 	}
 
 	shiftNodeDown(node) {
-		
+	//	while (node.priority < Math.max(node.left.priority, node.right.priority)) {
+	//	  if (node.left.priority == Math.max(node.left.priority, node.right.priority)) {
+	//			node.left.swapWithParent();
+	//		} else {node.right.swapWithParent();}
+		}
 	}
-}
+
 
 module.exports = MaxHeap;
--- src/node.js
+++ src/node.js
@@ -1,22 +1,57 @@
 class Node {
 	constructor(data, priority) {
-
+		this.data = data;
+		this.priority = priority;
+		this.parent = null;
+		this.left = null;
+		this.right = null;
 	}
 
 	appendChild(node) {
-
+		 var node_parent = this;
+     if (!this.left) {this.left = node; node.parent = this;} else
+			 if (!this.right) {this.right = node; node.parent = this;}
 	}
 
 	removeChild(node) {
-
+		 if (this.left == node) {this.left = null; node.parent = null;} else
+			 if (this.right == node) {this.right = null; node.parent = null;} else {
+				 throw new Error("Nothing to remove");
+			 }
 	}
 
 	remove() {
-
+     if (this.parent) {
+			 this.parent.removeChild(this);
+		 }
 	}
 
 	swapWithParent() {
-		
+		var swap_node = this;
+		var swap_parent = this.parent;
+		var swap_node_left = swap_node.left;
+		var swap_node_right = swap_node.right;
+		if (swap_parent) {
+		  if (swap_node.left) {swap_node.left.parent = swap_parent;}
+		  if (swap_node.right) {swap_node.right.parent = swap_parent;}
+		  swap_node.parent = swap_parent.parent;
+		  if (swap_node.parent) {
+		    if ((swap_parent.parent.left) && (swap_parent.parent.left == swap_parent)) {swap_parent.parent.left = swap_node;}
+		    else {swap_parent.parent.right = swap_node;}
+		  }
+		  if (swap_node == swap_parent.left) {
+		    swap_node.left = swap_parent;
+		    swap_node.right = swap_parent.right;
+		    if (swap_node.right) {swap_node.right.parent = swap_node;}
+		  } else {
+		    swap_node.right = swap_parent;
+		    swap_node.left = swap_parent.left;
+		    if (swap_node.left) {swap_node.left.parent = swap_node;}
+		  }
+		  swap_parent.parent = swap_node;
+		  swap_parent.left = swap_node_left;
+		  swap_parent.right = swap_node_right;
+		}
 	}
 }
 
